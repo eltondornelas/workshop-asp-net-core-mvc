f674136e0bedc679676584cb11e514d12d3fa7fa
Elton Dornelas <eltondornelas@gmail.com>
2019-08-11 14:29:59 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -15,9 +16,18 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
+        /*
         public List<Department> FindAll()
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+        //Modelo síncrono
+        */
+
+        public async Task<List<Department>> FindAllAsync() //esse sufixo é recomendado pela linguagem C#
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync(); //ToList é uma operação síncrona do Linq. a versão assíncrona é do entity framework e não do linq, precisa declarar a biblioteca: Microsoft.EntityFrameworkCore;
+            //necessário escrever o await para informar ao compilador que é assíncrona
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,30 +17,62 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
+        /*
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();
-        } //desse formato a operação é sincrona, ou seja, previsa esperar finalizar o processo para prosseguir
+            return _context.Seller.ToList(); //Acessa o banco de dados. Esse acesso é de forma lenta.
+            //desse formato a operação é sincrona, ou seja, precisa esperar finalizar o processo para prosseguir a aplicação.
+            //por esse motivo o ideal é que ela seja assíncrona para executar a operação de forma separada e a aplicação continua desbloqueada, permitindo assim a aplicação continuar funcionando.
+        }
+        */
+        public async Task<List<Seller>> FindAllAsync()
+        {
+            return await _context.Seller.ToListAsync(); 
+        }
 
+        /*
         public void Insert(Seller obj)
         {            
             _context.Add(obj);
             _context.SaveChanges();
         }
+        */
+        public async Task InsertAsync(Seller obj)
+        {
+            _context.Add(obj); //essa operação é feita somente em memória não precisa ser assíncrona.
+            await _context.SaveChangesAsync(); //essa operação é que efetivamente vai acessar o banco de dados, então é ela que necessita do async
+        }
 
+        /*
         public Seller FindById(int id)
         {
             //return _context.Seller.FirstOrDefault(obj => obj.Id == id);
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
+        */
+        public async Task<Seller> FindByIdAsync(int id)
+        {            
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
+        }
 
+        /*
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        */
+        public async Task RemoveAsync(int id)
+        {
+            //var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
+            _context.Seller.Remove(obj);
+            //_context.SaveChanges();
+            await _context.SaveChangesAsync();
+        }
 
+        /*
         public void Update(Seller obj)
         {
             if (!_context.Seller.Any(x => x.Id == obj.Id))
@@ -56,8 +88,28 @@ namespace SalesWebMvc.Services
             catch (DbConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
-            }
-            
+            }            
         }
+        */
+
+        public async Task UpdateAsync(Seller obj)
+        {
+            //bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id)
+            if (!await _context.Seller.AnyAsync(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
